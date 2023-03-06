fe78f903ba43f182044ad651d6e9591da0dd7a97
SamLyons <SamLyons@Samuels-MacBook-Air.local>
2023-03-06 11:11:48 -0500

    Added google mapsAPI and updated styling

--- app.js
+++ app.js
@@ -21,3 +21,14 @@ const changeNavButton = () => {
     navToggler.className = "fa-solid fa-xmark";
   }
 };
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
+
+window.initMap = initMap;
